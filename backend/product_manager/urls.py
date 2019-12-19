22b97ebd0ca3c78943e50b4b2ed9fccf334be187
raithand17 <andreas.raith@edu.fh-joanneum.at>
2019-12-19 13:31:20 +0100

    HW2 - Prepare backend with basic functionality

--- backend/product_manager/urls.py
+++ backend/product_manager/urls.py
@@ -1,9 +1,32 @@
+from django.conf.urls import url
 from django.urls import path
-from product_manager import views
+from drf_yasg import openapi
+from drf_yasg.views import get_schema_view
+
+from . import views
+
+from django.contrib import admin
+
+from rest_framework_jwt.views import obtain_jwt_token
+
+
+
+schema_view = get_schema_view(
+    openapi.Info(
+        title='API',
+        default_version='v1'
+    ),
+)
 
 urlpatterns = [
     path('products/', views.product_list),
     path('products/<int:id>/', views.product_update),
     path('producer/', views.producer_list),
     path('warehouse/', views.warehouse_list),
+
+    url(r'^swagger(?P<format>\.json|\.yaml)$', schema_view.without_ui(cache_timeout=0), name='schema-json'),
+    url(r'^swagger/$', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
+    url(r'^redoc/$', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc'),
+
+    url(r'^api-token-auth/', obtain_jwt_token),
 ]
